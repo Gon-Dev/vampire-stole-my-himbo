f96694987414e2c0b8c5ccb0dae952087ecd0cf0
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2023-04-21 20:56:54 -0300

    fix: open about links in new tab

--- src/components/Footer/About/DescriptionPanel/DescriptionPanel.tsx
+++ src/components/Footer/About/DescriptionPanel/DescriptionPanel.tsx
@@ -1,11 +1,10 @@
 import styles from "./descriptionPanel.module.css";
 function DescriptionPanel( {isExpanded}: {isExpanded: boolean} ) {
-  console.log();
   return (
     <div className={styles.container} style={isExpanded ? {transform: "translateX(0px)"}:{transform: `translateX(${window.innerWidth / 2}px)`}}>
       <p className={styles.description}>{"v0.0.1 (beta)"}</p>
       <p className={styles.description}>{"Developed by Goni. More about me "} <a className={styles.link} target="_blank" href="https://gonzaloruiz.ar/">here</a>.</p>
-      <p className={styles.description}>{"Check out more one page RPGs at Oliver's"} <a className={styles.link} href="https://www.patreon.com/deathbybadger">patreon</a> profile.</p>
+      <p className={styles.description}>{"Check out more one page RPGs at Oliver's"} <a className={styles.link} target="_blank" href="https://www.patreon.com/deathbybadger">patreon</a> profile.</p>
     </div>
   );
 }
